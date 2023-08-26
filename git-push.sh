a9c1d09472e39161eca3380dda66e47f5c8b3043
ShauryaYamdagni.com <shauryayamdagni0@gmail.com>
2023-08-27 04:05:45 +0530

    first

---
+++ git-push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -r gitToken < /home/socrates/scripts/.secret.git.txt
+
+echo $gitToken
+$(git init)
+$(git add * )
+$( git commit * -m "first" )
+
+gitUrl="github.com/shauryaCodesAndHosts/Kalvium_task.git"
+
+gitUserName="shauryaCodesAndHosts"
+
+final_url="https://$gitUserName:$gitToken@$gitUrl"
+
+echo $final_url
+
+repo_url= "https://$gitUrl"
+
+
+
+echo $repo_url
+
+# $(git push -u origin master)
+
+echo $(git remote set-url origin $final_url)
+
+$(git push origin master)
+
